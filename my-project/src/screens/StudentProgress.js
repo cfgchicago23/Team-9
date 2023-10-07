8723cd104eda92ad6faf07ea5b6e33fdcb093eeb
Lakshmi Damodaran <luckylvd2003@gmail.com>
2023-10-07 04:02:06 -0500

    finished frontend for leader board

--- my-project/src/screens/StudentProgress.js
+++ my-project/src/screens/StudentProgress.js
@@ -3,18 +3,18 @@ import React from 'react';
 import { View } from 'react-native';
 import MyProgressBar from './MyProgressBar';
 
-function App() {
+function Progress() {
   return (
     <View>
       <body>Lesson 1</body>
       <MyProgressBar progress={0.8} width={200} height={20} color="#E37383" borderWidth={2} />
       <body>Lesson 2</body>
-      <MyProgressBar progress={0.5} width={250} height={15} color="#E37383"borderWidth={2} />
+      <MyProgressBar progress={0.5} width={250} height={15} color="#E37383" borderWidth={2} />
       <body>Lesson 3</body>
-      <MyProgressBar progress={0.3} width={250} height={15} color="#E37383"borderWidth={2} />
+      <MyProgressBar progress={0.3} width={250} height={15} color="#E37383" borderWidth={2} />
       
     </View>
   );
 }
 
-export default App;
+export default Progress;
