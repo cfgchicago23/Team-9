29ad94e896bd12a2708fb9ccadd411e4d79b3d98
nishthadalvi <nishthadalvi@utexas.edu>
2023-10-07 00:34:50 -0500

    taking out backend access from client-side

--- my-project/src/helpers/emailValidator.js
+++ my-project/src/helpers/emailValidator.js
@@ -1,12 +1,6 @@
-use('cfgteam9.tpdepmo.mongodb.net');
-
 export function emailValidator(email, userType) {
-    //const re = /\S+@\S+\.\S+/
+    const re = /\S+@\S+\.\S+/
     if (!email) return "Please fill in this field."
-    if(userType == "Student"){
-        return ''
-    }
-
-    //if (!re.test(email)) return 'Please enter a valid email address!'
+    if (!re.test(email)) return 'Please enter a valid email address!'
     return ''
 }
